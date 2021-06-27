0dcd24cae01297aa3dd1497ebfe5ebf5593050a8
simoneliskases <simon.eliskases@outlook.de>
2021-06-27 21:07:34 +0200

    Added Calculation Generation

---
+++ Assets/MyGame/Scripts/Calculation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculation : MonoBehaviour
+{
+    public static Vector3 Addition(int maxSum)
+    {
+        int z = Random.Range(1, maxSum);
+        int x = Random.Range(1, z - 1);
+        int y = z - x;
+        return new Vector3(x, y, z);
+    }
+}
---
+++ Assets/MyGame/Scripts/GameUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameUI : MonoBehaviour
+{
+    public int maxSum;
+    public TextMeshProUGUI xText;
+    public TextMeshProUGUI yText;
+    public TextMeshProUGUI zText;
+
+    private void OnEnable()
+    {
+        Vector3 values = Calculation.Addition(maxSum);
+        int calculationIndex = Random.Range(0, 3);
+        // 0: x + ? = z
+        // 1: x + y = ?
+        // 2: ? + ? = z
+
+        switch (calculationIndex)
+        {
+            case 0:
+                xText.text = values.x.ToString();
+                zText.text = values.z.ToString();
+                break;
+            case 1:
+                xText.text = values.x.ToString();
+                yText.text = values.y.ToString();
+                break;
+            case 2:
+                zText.text = values.z.ToString();
+                break;
+        }
+    }
+}
