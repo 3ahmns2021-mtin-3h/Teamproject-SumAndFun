07054e0e5acbda3e0c6e0318e3f0b04b8bfc0ac0
simoneliskases <simon.eliskases@outlook.de>
2021-06-28 13:37:47 +0200

    Tried to Fix Drag and Drop Bug

--- Assets/MyGame/Scripts/DragAndDrop.cs
+++ Assets/MyGame/Scripts/DragAndDrop.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DragAndDrop : MonoBehaviour
+public class DragAndDrop : UIElement
 {
     private Camera cam;
-    private Coroutine currentDrag;
+    private GraphicRaycaster raycaster;
+    private EventSystem eventSystem;
     private float distance;
 
     private void Awake()
     {
         cam = Camera.main;
+        raycaster = GetComponent<GraphicRaycaster>();
+        eventSystem = GetComponent<EventSystem>();
     }
 
     private void Update()
@@ -18,24 +23,20 @@ public class DragAndDrop : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             distance = Vector3.Distance(transform.position, cam.transform.position);
-            currentDrag = StartCoroutine(Drag());
+
+            if(currentDrag == null)
+            {
+                currentDrag = StartCoroutine(Drag(raycaster, eventSystem, cam, distance));
+            }
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            StopCoroutine(currentDrag);
-        }
-    }
-
-    private IEnumerator Drag()
-    {
-        while (true)
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            Vector3 rayPoint = ray.GetPoint(distance);
-
-            transform.position = rayPoint;
-            yield return null;
+            if(currentDrag != null)
+            {
+                StopCoroutine(currentDrag);
+                currentDrag = null;
+            }
         }
     }
 }
